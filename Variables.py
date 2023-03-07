08359b2a39bb14a2795f9c2d2e4590a6703aa588
ZaydBerr <127031186+ZaydBerr@users.noreply.github.com>
2023-03-07 18:16:51 +0100

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,8 @@
+# A Variable is a small folder for a Text, a Number, you can use too 0 or 1!
+string = "text"
+integer = 01001110 01110101 01101101 01100010 01100101 01110010 01110011
+# Look for it inthe web!
+# That 0s and 1s means Number!
+boolean = true
+character = 'A'
+floatingNumber = 0
